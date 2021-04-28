1a862b12ddf5d603a8a12e6b39c533741d7adf37
ReformerzAplus <liaohtc355313@gmail.com>
2021-04-29 07:46:08 +0800

    Q1 & Q2

--- no_2.py
+++ no_2.py
@@ -36,28 +36,21 @@ def getAllData(url):
             sleep(1) # seconds
             windows = driver.window_handles
             driver.switch_to.window(windows[-1])
-            
             ###地區
             location = driver.execute_script("return document.querySelectorAll('#propNav a')[2].innerText")
-
             ###出租者
-            owner = driver.execute_script("return document.querySelector(\".avatarRight div i\").innerText")
-           
+            owner = driver.execute_script("return document.querySelector(\".avatarRight div i\").innerText")         
             ###出租者身份
             author = (driver.execute_script("return document.querySelector('.avatarRight div').innerText")).split()[1]
             is_Owner = re.search('(屋主)',author)
-
             ###聯絡電話
-            phone = driver.execute_script('return document.querySelector(".dialPhoneNum").getAttribute("data-value")')
-            
+            phone = driver.execute_script('return document.querySelector(".dialPhoneNum").getAttribute("data-value")')         
             ###房屋型態：
             search_house_type = (driver.execute_script("return document.querySelector('.attr').innerText"))
             house_type = re.search('(透天厝)|(電梯大樓)|(公寓)',search_house_type)
-
             ###現況
             search_now_type = (driver.execute_script("return document.querySelector('.attr').innerText"))
             now_type = re.search('(分租套房)|(整層住家)|(車位)|(獨立套房)|(雅房)|(其他)',search_house_type)
-
             ###性別要求
             search_male_or_female_type = (driver.execute_script("return document.querySelector('ul.clearfix.labelList.labelList-1').innerText"))
             sex_type = re.search('(男女生皆可)|(男生)|(女生)',search_male_or_female_type)
@@ -65,17 +58,16 @@ def getAllData(url):
             ht = house_type.group(0) if house_type is not None else None
             st = sex_type.group(0) if sex_type is not None else None
             nt = now_type.group(0) if now_type is not None else None
-            nr = is_Owner.group(0) if is_Owner is not None else "非屋主"
-            
+            nr = is_Owner.group(0) if is_Owner is not None else "非屋主"          
+
             data = {"location": location , "HouseOwner": owner , "HouseOwner_author": nr , "Phone": phone , "House_Type": ht , "Now_Type": nt , "Sex": st}
             print(data)
-            insertData(data)
-          
+            insertData(data)        
             ###關閉分頁
             driver.close()
             driver.switch_to.window(windows[0])
             sleep(1) # seconds
-        
+            
         #換到下一頁
         driver.execute_script("document.querySelector('.pageBar a.pageNext').click()")
         sleep(.8) #seconds
